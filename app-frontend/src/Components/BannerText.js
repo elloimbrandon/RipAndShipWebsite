f050c14027a1c4c9bbc91c5aebce096b914eb3b3
Brandon Feltz <elloimbrandon@gmail.com>
2022-08-15 15:04:38 -0700

    tiding up nav and adding banner text

---
+++ app-frontend/src/Components/BannerText.js
@@ -0,0 +1,9 @@
+const BannerText = () => {
+  return (
+    <div>
+      <h1>Banner Text</h1>
+    </div>
+  );
+};
+
+export default BannerText;
--- app-frontend/src/Components/NavBar.js
+++ app-frontend/src/Components/NavBar.js
@@ -4,6 +4,7 @@ import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import { useState } from "react";
+import BannerText from "./BannerText";
 
 const NavBar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
@@ -15,40 +16,46 @@ const NavBar = () => {
     setAnchorEl(event.currentTarget);
   };
   return (
-    <div className="mb-28">
-      <AppBar position="fixed" className="border-t border-r border-l w-screen">
-        <Toolbar className="h-20">
-          <Box display="flex" flexGrow={1}>
-            <div>
-              <h1>PokemonKidwiththeHits</h1>
-              <h1>Hello</h1>
-            </div>
-          </Box>
-          <Button variant="contained" onClick={openMenu}>
-            <MenuIcon />
-          </Button>
-          <Menu
-            id="lame-menu"
-            anchorEl={anchorEl}
-            KeepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-            className="mt-2"
-          >
-            {/* do I need handle close here? */}
-            <MenuItem>
-              <Button
-                href="/inventory"
-                variant="textSuccess"
-                endIcon={<InventoryIcon />}
-              >
-                Inventory
-              </Button>
-            </MenuItem>
-          </Menu>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <>
+      <div className="mb-28">
+        <AppBar
+          position="fixed"
+          className="border-t border-r border-l w-screen"
+        >
+          <Toolbar className="h-20">
+            <Box display="flex" flexGrow={1}>
+              <div>
+                <h1>PokemonKidwiththeHits</h1>
+                <h1>Hello</h1>
+              </div>
+            </Box>
+            <Button variant="contained" onClick={openMenu}>
+              <MenuIcon />
+            </Button>
+            <Menu
+              id="lame-menu"
+              anchorEl={anchorEl}
+              KeepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              className="mt-2"
+            >
+              {/* do I need handle close here? */}
+              <MenuItem>
+                <Button
+                  href="/inventory"
+                  variant="textSuccess"
+                  endIcon={<InventoryIcon />}
+                >
+                  Inventory
+                </Button>
+              </MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </div>
+      <BannerText />
+    </>
   );
 };
 
